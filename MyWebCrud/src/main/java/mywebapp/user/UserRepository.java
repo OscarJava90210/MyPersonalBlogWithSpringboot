c9a3fa2e47d8948b4df9e2993b89bed0f3b902d4
OscarJava90210 <ashatbek572@gmail.com>
2022-06-24 23:12:01 +0500

    loginpage first

--- MyWebCrud/src/main/java/mywebapp/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package mywebapp.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    public Long countUserById(Integer id);
-}
